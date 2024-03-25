71089f435e44f40c9b93fe096bfac7fb6e1adee0
hunain-abbas <hunain.accunity@gmail.com>
2024-03-25 11:53:13 +0500

    Credentials Updated

--- src/pages/api/contact.js
+++ src/pages/api/contact.js
@@ -14,7 +14,7 @@ export default async function handler(req, res) {
         secure: false,
         auth: {
           user: 'website.mailer@rayn.group',
-          pass: 'jepfsuddryutdmf'
+          pass: 'vkvnhgwfwepenlly'
         }
       });
 
